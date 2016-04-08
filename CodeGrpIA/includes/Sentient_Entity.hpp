2f8d9cedba7b158ce86ccd6a1eae96053af5993e
temjanika-perkova <temjanika@gmail.com>
2016-04-08 09:53:30 +0200

    implementation de Sentitent_Entity
    to be continued :)

--- CodeGrpIA/includes/Sentient_Entity.hpp
+++ CodeGrpIA/includes/Sentient_Entity.hpp
@@ -8,10 +8,32 @@
 
 class Sentient_Entity : public Entity{
 
-	public : 
-		
+	public :
+
 		Sentient_Entity(int, bool, int, int , int ,int, int , int, int, unsigned int, std::string);
-		
+//------------------ Accesseurs -----------------------------------------
+		int getVitality();
+		bool getGender();
+		int getAge();
+		int getAgressiveness();
+		int getFitness();
+		int getSatiety();
+		int getSatietyTreshold();
+		int getReflex();
+		int getPerception();
+//---------------- Mutateurs --------------------------------------------
+        void setVitality(int v);
+        void setGender(bool g);
+        void setAge (int a);
+        void setAgressiveness(int ag);
+        void setFitness(int f);
+        void setSatiety(int s);
+        void setSatietyTreshold(int st);
+        void setReflex (int r);
+        void setPerception (int p);
+
+        bool isHungry();
+        bool isAgressive();
 		//destructor ?
 
 	private :
@@ -24,9 +46,9 @@ class Sentient_Entity : public Entity{
 		int satiety_threshold;
 		int reflex;
 		int perception;
-		//job 
-	
-	
+		//Complex_Behaviour job;
+
+
 
 };
 
--- CodeGrpIA/src/Sentient_Entity.cpp
+++ CodeGrpIA/src/Sentient_Entity.cpp
@@ -2,3 +2,71 @@
 #include "Sentient_Entity.hpp"
 
 Sentient_Entity::Sentient_Entity(int v, bool g, int a, int agr , int f ,int s, int s_t, int r, int p, unsigned int i, std::string n) : Entity(i, n){}
+
+int Sentient_Entity::getVitality(){
+    return vitality;
+}
+int Sentient_Entity::getGender(){
+    return gender;
+}
+int Sentient_Entity::getAge(){
+    return age;
+}
+int Sentient_Entity::getAgressiveness(){
+    return agressiveness;
+}
+int Sentient_Entity::getFitness(){
+    return fitness;
+}
+int Sentient_Entity::getSatiety(){
+    return satiety;
+}
+int Sentient_Entity::getSatietyTreshold(){
+    return satiety_threshold;
+}
+int Sentient_Entity::getReflex(){
+    return reflex;
+
+}
+int Sentient_Entity::getPerception(){
+    return perception;
+}
+
+void Sentient_Entity::setVitality(int v){
+    vitality=v;
+}
+void Sentient_Entity::setGender(bool g){
+    gender=g;
+}
+void Sentient_Entity::setAge(int a){
+    age=a;
+}
+void Sentient_Entity::setFitness(int f){
+    fitness=f;
+}
+void Sentient_Entity::setAgressiveness(int ag){
+    agressiveness=a;
+}
+void Sentient_Entity::setSatiety(int s){
+    satiety=s;
+}
+void Sentient_Entity::setSatietyTreshold(int st){
+    satiety_threshold=st;
+}
+void Sentient_Entity::setReflex(int r){
+    reflex=r;
+}
+void Sentient_Entity::setPerception(int p){
+    perception=p;
+}
+
+bool Sentient_Entity::isHungry(){
+    if(satiety==satiety_threshold)
+        return true;
+    return false;
+}
+bool Sentient_Entity::isAgressive(){
+    if(agressiveness>50)
+        return true;
+    return false;
+}

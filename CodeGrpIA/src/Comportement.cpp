f20c8f8189e768542c9aa5e43d06ec6359d0a1dd
William <williamraofernandes@gmail.com>
2016-04-11 11:12:49 +0200

    implem comportement/noeuds.cpp

--- CodeGrpIA/src/Comportement.cpp
+++ CodeGrpIA/src/Comportement.cpp
@@ -1,1 +1,13 @@
-// #include "Include.hpp"
+#include "Comportement.hpp"
+
+Comportement::Comportement(vector<Noeud*> vN, Noeud * nD, vector<Noeud*> nFs) : noeuds(vN), noeudDepart (nD), noeudsFin(nFs) {}
+
+void Comportement::AjouterNoeud(Noeud * n){
+    noeuds.push_back(n);
+}
+
+void Comportement::SupprimerNoeud(Noeud * n){
+    for (std::vector<Noeud *>::iterator it = noeuds.begin() ; it != noeuds.end(); ++it){
+        if(*it == n) noeuds.erase(it);
+    }
+}

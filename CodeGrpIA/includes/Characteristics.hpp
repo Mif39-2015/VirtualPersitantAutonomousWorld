70f2e68ce13fb8af88dc9c18ae6240d73fb1e02c
anomark89 <anomark89@gmail.com>
2016-04-11 16:31:35 +0200

    ajout de Characteristics.hpp

---
+++ CodeGrpIA/includes/Characteristics.hpp
@@ -0,0 +1,49 @@
+#ifndef Characteristics_hpp
+#define Characteristics_hpp
+
+#include <vector>
+
+
+class Characteristics{
+
+public:
+
+	//les characteristiques provenant du fichier
+	static std::vector<Characteristics> listCharacteristics;
+	
+	//cnstruit un vector de charactéristiques à partir du fichier donné
+	std::vector<Characteristics> loadCharacteristics(std::string fileName);
+
+
+	int getId();
+	int getMin();
+	int getMax();
+	std::string getName();
+	std::string getDefinition();
+	int getPrecision();
+	
+	void setId(int i);
+	void setMin(int m);
+	void setMax(int m);
+	void setName(std::string n);
+	void setDefinition(std::string d);
+	void setPrecision(int p);
+	
+	
+	
+private:
+	int id;
+	int min;
+	int max;
+	std::string name;
+	std::string definition;
+	int precision;
+
+
+
+};
+
+
+
+
+#endif //Characteristics.hpp

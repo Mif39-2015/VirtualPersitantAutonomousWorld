ff2ebc50374026ae4a0c4603d65b548332817e3d
William <williamraofernandes@gmail.com>
2016-04-11 11:03:15 +0200

    implem comportement/noeuds.cpp

---
+++ codeGrpIA/src/Noeud.cpp
@@ -0,0 +1,18 @@
+#include "Noeud.hpp"
+
+Noeud::Noeud() {}
+Noeud::Noeud(Action* a) : action(a){}
+Noeud::Noeud(Noeud* n) : arretesOut(n->arretesOut), arretesIn(n->arretesIn), action(n->action){}
+
+void Noeud::setAction(Action * a) {
+    action = a;
+}
+
+Action * Noeud::getAction(){
+    return action;
+}
+ 
+Noeud * Noeud::executerNoeud(Agent * a){
+    for (std::vector<Arrete *>::iterator it = arretesOut.begin() ; it != arretesOut.end(); ++it)
+        if
+}

de703dbe2270e0e8737727b6509161b11a754b27
Jaglika Perkova <jaglika.perkova@etu.univ-lyon1.fr>
2016-04-12 09:29:42 +0200

    commentaires dans sentient_entity

--- CodeGrpIA/includes/Sentient_Entity.hpp
+++ CodeGrpIA/includes/Sentient_Entity.hpp
@@ -11,19 +11,52 @@ class Sentient_Entity : public Entity{
 
 public:
 
-Sentient_Entity(int, int, std::string, std::string);
+	/*
+	* C'est un constructeur avec 3 parametres oú le premier entier c'est la
+	* position de X, le deuxieme c'est la position Y et le troisime c'est
+	* le nom de sentient entity concerné
+	* */	
+	Sentient_Entity(int, int, std::string, std::string);
+	/*
+	* Ce methode renvoie la valeur de la clé id de la map.
+	* Si la clé existe, sa valeur associée est renvoyée, sinon -1
+	* */	
     int getVal(int id);
+	/*
+	* Ce methode met à jour la valeur associée à la clé id si cette derniere
+	* existe bien dans la map et renvoie 1 (success) sinon renvoie -1
+	* */
     int setVal(int id, int v);
+	/*
+	* Ce methode renvoie la position X du sentient entity
+	* */
     int getPosX();
+	/*
+	* Ce methode renvoie la postion Y du sentient entity
+	* */
     int getPosY();
+	/*
+	* Ce methode met la valeur de la position de X du sentient entity
+	* */
     void setPosX(int _x);
+	/*
+	* Ce methode met la valeur de la postion de Y du sentient entity
+	* */
     void setPosY(int _y);
 
 protected:
+	/*
+	* Cet attribut represente la postion de X du sentient entity
+	* */
     int posX;
+	/*
+	* Cet attribut represente la position de Y du sentient entity
+	* */
     int posY;
-
-    //idc int c'est l'id de la caractéristique et val 2eme est l'id de ce cen
+    /*
+     * Cet attribut represente un map de correspondance entre la caracteristique et le sentient
+     * entity. 
+     * */
     std::map<int, int> charact_correspondence;
 
 

b8a5cf7c5bae711716fbffb6ab94a01b9f547ae6
Bruno Dumas <dumas.bru@gmail.com>
2016-04-12 15:43:34 +0200

    Base du projet (serveur)

---
+++ src/AuthenticationModule.cpp
@@ -0,0 +1,22 @@
+/**
+ * \file AuthenticationModule.cpp
+ * \brief Implementation of AuthenticationModule.hpp
+ */
+#include "AuthenticationModule.hpp"
+
+
+AuthenticationModule::AuthenticationModule(){
+	
+}
+
+int AuthenticationModule::registerClient(string username, string password){
+	
+}
+
+bool AuthenticationModule::removeClient(string username, string password){
+	
+}
+
+bool AuthenticationModule::authClient(string username, string password){
+	
+}

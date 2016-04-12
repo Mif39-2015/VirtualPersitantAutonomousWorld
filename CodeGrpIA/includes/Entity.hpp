978f0a1ca31001609543dd5a90ae701b356d1a64
R.I.P <rip-geek@hotmail.fr>
2016-04-12 22:27:55 +0200

    Finalisation de isTrue pour la Condition telle qu'elle est définie

--- CodeGrpIA/includes/Entity.hpp
+++ CodeGrpIA/includes/Entity.hpp
@@ -11,31 +11,32 @@ using namespace std;
 
 class Entity{
 
-protected:
-	static unsigned int idCount;
-	unsigned int id;
-	string typeId;
-	string name;
-	map<Item, unsigned int> inventory;
-	Position pos;
-        bool modif;
+	protected:
+		static unsigned int idCount;
+		unsigned int id;
 
-public:
+		string typeId;
+		string name;
+		map<Item, unsigned int> inventory;
+		Position pos;
+		bool modif;
 
-	Entity(std::string, std::string);
+	public:
 
-	unsigned int getId();
-	string getName();
-	Position getPos();
-        bool getModif();
-	map<Item, unsigned int> getInventory();
-    	string getTypeId();
-        void setModif(bool m);
-	void setName(std::string n);
-	void setPos(int x, int y);
-	void setInventory(std::map<Item, unsigned int> inv);
+		Entity(std::string, std::string);
 
-	~Entity(void);
+		unsigned int getId();
+		string getName();
+		Position getPos();
+		bool getModif();
+		map<Item, unsigned int> getInventory();
+		string getTypeId();
+		void setModif(bool m);
+		void setName(std::string n);
+		void setPos(int x, int y);
+		void setInventory(std::map<Item, unsigned int> inv);
+
+		~Entity(void);
 };
 
 #endif
--- CodeGrpIA/src/Arete.cpp
+++ CodeGrpIA/src/Arete.cpp
@@ -24,5 +24,32 @@ bool Arete::isTrue(Sentient_Entity * a){
 	for(auto it=conditions_Simples.begin(); it!=conditions_Simples.end(); ++it){
 		if(!FonctionCondition::mapFonctionsBool[*it](a)) return false;
 	}
+	for(auto it=condition_Complexes.begin(); it!=condition_Complexes.end(); ++it){
+		int val1 = FonctionCondition::mapFonctionsInt[it->fonc1](a,it->e1);
+		int val2;
+		if(it->fonc2!=FONC_NULL)
+			val2 = FonctionCondition::mapFonctionsInt[it->fonc2](a,it->e2);
+		else
+			val2 = it->comp;
+		switch (it->op){
+			case Inf :
+				if(!val1<val2) return false;
+				break;
+			case Inf_Eg :
+				if(!val1<=val2) return false;
+				break;
+			case Eg :
+				if(!val1==val2) return false;
+				break;
+			case Sup_Eg :
+				if(!val1>=val2) return false;
+				break;
+			case Sup :
+				if(!val1>val2) return false;
+				break;
+			default :
+				break;
+		}
+	}
 	return true;
 }

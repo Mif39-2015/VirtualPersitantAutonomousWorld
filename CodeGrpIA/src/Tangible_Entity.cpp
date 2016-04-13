16c670b0f46af834b07d4cf5d276b0f951092365
Victor et William <>
2016-04-13 11:11:05 +0200

    Suppression anciens fichiers + modif makefile

--- CodeGrpIA/src/Tangible_Entity.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "Tangible_Entity.hpp"
-
-Tangible_Entity::Tangible_Entity(std::string s, std::string s2) :
-    Entity(s, s2) {
-}
--- CodeGrpIA/src/Vector3.cpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-//~ #include "Triple.hpp"
-
-// Triple::Triple(int a, int b, int c) : x(a), y(b), z(c){}

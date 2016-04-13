ae3d88af9a496c71984aa9d0d4076d8334cf2f58
grange <martin.grange@etu.univ-lyon1.fr>
2016-04-13 14:37:41 +0200

    fonction d'ajout d'utilisateur

--- src/db/sqliteInterface.hpp
+++ src/db/sqliteInterface.hpp
@@ -11,6 +11,7 @@ class SQLiteAccess{
 		~SQLiteAccess();
 
 		int sqlRequest(std::string sqlStatement);
+		int sqlAddUser(std::string username, std::string password, std::string addmail);
 };
 
 static int callback(void *data, int argc, char **argv, char **azColName);

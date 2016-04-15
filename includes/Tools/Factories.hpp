584480fe18e5ebbc2ea8cd10cda629eb4b851bee
Alix <alix.gonnot@gmail.com>
2016-04-15 10:02:48 +0200

    suppression de la lib msgpack-c et mise en place du doxyfile

--- includes/Tools/Factories.hpp
+++ includes/Tools/Factories.hpp
@@ -1,6 +1,12 @@
 #ifndef FACTORIES_HPP
 #define FACTORIES_HPP
 
+/*!
+ * \file Factories.hpp
+ * \brief contient la classe Factories
+ * \author Groupe IA
+ */
+
 #include <fstream>
 #include <iostream>
 #include "Entity/Characteristics.hpp"
@@ -9,18 +15,30 @@
 #include "Entity/Item.hpp"
 #include "Tools/Tools.hpp"
 
+/*! \class Factories 
+   * \brief Classe contenant les outils permettant d'instancier les différentes entités
+   */
 class Factories{
 
 public:
-
-	//Factories permettant de créer les entités conscientes
-
+	/*!
+     	*  \brief Factory permettant d'instancier les Agents
+     	*/
 	static Sentient_Entity * createAgent();
 	
+	/*!
+     	*  \brief Factory permettant d'instancier les Animaux
+     	*/
 	static Sentient_Entity * createAnimal();
 	
+	/*!
+     	*  \brief Factory permettant d'instancier les Items
+     	*/
 	static Item * createItem();
 	
+	/*!
+     	*  \brief Factory permettant d'instancier les Clans
+     	*/
 	static Tribe * createTribe();
 
 };
--- includes/Tools/Position.hpp
+++ includes/Tools/Position.hpp
@@ -2,17 +2,20 @@
 #define POSITION_HPP
 
 class Position{
-public:
-	Position(int, int);
-	Position(const Position&);
-	~Position(void);
-        
-        int getX() const;
-        int getY() const;
-        bool isInCircle(Position* p, int rayon);
 
-private:
-	int x;
-	int y;
+	public:
+		Position(int, int);
+		Position(const Position&);
+		int getX() const;
+		int getY() const;
+		bool isInCircle(Position* p, int rayon);
+		~Position(void);
+		
+		
+
+	private:
+		int x; /*!<Coordonnées x de la position*/
+		int y; /*!<Coordonnées y de la position*/
+		
 };
 #endif

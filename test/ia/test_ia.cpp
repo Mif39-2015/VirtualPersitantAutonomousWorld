177efb260a2599825e1d948a9606e46eb1d93a64
Jaglika Perkova <jaglika.perkova@etu.univ-lyon1.fr>
2016-04-15 13:52:19 +0200

    ajout test pour Tribe

--- test/ia/test_ia.cpp
+++ test/ia/test_ia.cpp
@@ -274,16 +274,51 @@ TEST_CASE("Test IA", "[ia]")
 	{
 		cout << "TEST ITEM" << endl;
 		int loadResult = ItemList::loadItemsFile("../../../data/descriptionItems.txt");
-		
+
 		REQUIRE(ItemList::listItems.size() != 0);
-		
+
 		Tribe* t = Factories::createTribe();
 		REQUIRE(t != NULL);
-		
+
 		t->addItemToStock(&(ItemList::listItems[0]), 3);
-		
+
 		REQUIRE(t->getQuantityByItem(&(ItemList::listItems[0])) == 3);
-		
+
 		t->afficheStock();
+
+
+	}
+	SECTION("TEST TRIBE")
+	{
+		cout << "TEST TRIBE" << endl;
+		int loadResult = ItemList::loadItemsFile("../../../data/descriptionItems.txt");
+
+		REQUIRE(ItemList::listItems.size() != 0);
+
+		Tribe* t = Factories::createTribe();
+		REQUIRE(t != NULL);
+		t->setName("Anishinaabe");
+		REQUIRE(t->getName()=="Anishinaabe");
+		t->addItemToStock(&(ItemList::listItems[1]), 5);
+
+		REQUIRE(t->getQuantityByItem(&(ItemList::listItems[1])) == 5);
+		t->addItemToStock(&(ItemList::listItems[0]), 10);
+		cout<<"Le stock du tribu "<< t->getName() <<" :" << endl;
+		t->afficheStock();
+
+		Sentient_Entity * agent = Factories::createAgent();
+		cout << "on a créé l'agent " << endl;
+		Sentient_Entity * animal = Factories::createAnimal();
+		cout << "on a créé l'animal " << endl;
+		t->addNewEntity(agent);
+		cout<<"on l'a ajouté l'agent dans le tribe" << endl;
+		t->addNewEntity(animal);
+		cout<<"on l'a ajouté l'animal dans le tribe" << endl;
+		cout<<"le nb des entités dans tribe: "<< t->getTribeSize() << endl;
+
+		cout<<"le type de l'entité: " <<agent->getTypeId() <<" -- le type de agent2: "<<animal->getTypeId() <<endl;
+
+
+
 	}
 }

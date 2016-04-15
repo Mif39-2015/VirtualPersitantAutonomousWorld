deb575d96ff775d8d9d03d356b51921a71a17f36
Alix <alix.gonnot@gmail.com>
2016-04-15 14:09:11 +0200

    debut commentaires package Behavior

--- includes/Behavior/ActionComportement.hpp
+++ includes/Behavior/ActionComportement.hpp
@@ -1,6 +1,12 @@
 #ifndef ACTIONCOMPORTEMENT_HPP
 #define ACTIONCOMPORTEMENT_HPP
 
+/*!
+ * \file ActionComportement.hpp
+ * \brief contient la classe ActionComportement
+ * \author Groupe IA
+ */
+
 using namespace std;
 
 #include "Behavior/Action.hpp"
@@ -8,19 +14,37 @@ using namespace std;
 
 class Comportement;
 
+/*!
+*\class ActionComportement 
+*\brief Classe décrivant les actions contenant un comportement
+*/
 class ActionComportement : public Action {
 
 	private :
-		Comportement * comportement;
+		Comportement * comportement;/*!<comportement composant l'action'*/
 
 	public :
-		void Executer(Sentient_Entity * agent);
-
+	
+		
+		/*!
+	     	*\brief Constructeur par défaut
+	     	*/
 		ActionComportement(){comportement = nullptr;};
+		
+		/*!
+	     	*\brief Constructeur avec paramètre
+	     	*\param c : comportement qui va composer l'action
+	     	*/
 		ActionComportement(Comportement * c) : comportement(c) {};
 
 		Comportement * getComportement(){return comportement;}
 		void setComportement(Comportement * c){comportement = c;}
+		
+		/*!
+	     	*\brief fait éxecuter le comportement courant un agent
+	     	*\param agent : agent qui éxecute l'action'
+	     	*/
+		void Executer(Sentient_Entity * agent);
 
 };
 

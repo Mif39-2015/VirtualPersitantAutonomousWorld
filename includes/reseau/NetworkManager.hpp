7343d1e8e3e9d6a3384e8f20a7bec5aef4df2492
JeremyBesson <jbesson022@gmail.com>
2016-04-18 11:10:18 +0200

    Fix Pb compile + NetworkManager todo

---
+++ includes/reseau/NetworkManager.hpp
@@ -0,0 +1,18 @@
+/**
+ * \file NetworkManager.hpp
+ * \brief NetworkManager class definition
+ */
+#ifndef _NETWORKMANAGER_
+#define _NETWORKMANAGER_
+
+/**
+ * \class NetworkAdapter
+ * \brief Link with the network block
+ */
+class NetworkAdapter {
+	private:
+		
+		vector<Client*> clients;
+};
+
+#endif

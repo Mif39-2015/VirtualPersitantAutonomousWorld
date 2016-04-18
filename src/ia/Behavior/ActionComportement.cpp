5cb84eb42045628a5ceb8bfc72100d77b47d053f
R.I.P <rip-geek@hotmail.fr>
2016-04-18 10:24:03 +0200

    CleanUp de Code de Behavior

--- src/ia/Behavior/ActionComportement.cpp
+++
@@ -1,6 +0,0 @@
-#include "Behavior/Comportement.hpp"
-#include "Behavior/ActionComportement.hpp"
-
-void ActionComportement::Executer(Sentient_Entity * agent) {
-	agent->addToTrace(comportement, comportement->getNoeudDepart(), true);
-}

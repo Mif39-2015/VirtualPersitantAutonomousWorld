70450b09f5fc3c4e2d66747d7439dcd1d83de622
Bruno <dumas.bru@gmail.com>
2016-04-21 10:40:03 +0200

    Raccourcis commandes + fix stop/pause/save/load

--- src/server/WorldSimulator.cpp
+++ src/server/WorldSimulator.cpp
@@ -61,14 +61,11 @@ void WorldSimulator::worldRun(){
 		if(state == SimulationState::PAUSED_SIMULATION){
 			unique_lock<mutex> lk(guardMutex);
         	guardCV.wait(lk);
-		}
-
-		if(state == SimulationState::REQUIRE_WAIT_SIMULATION){
+		} else if(state == SimulationState::REQUIRE_WAIT_SIMULATION){
 			// Signal the user commands handler thread that no more actions are being performed
 			// so it can start the saving procedure
 			{
 				lock_guard<mutex> lk(guardMutex);
-				state = SimulationState::WAITING_SIMULATION;
 			}
 
 			guardCV.notify_one();
@@ -77,68 +74,68 @@ void WorldSimulator::worldRun(){
 				unique_lock<mutex> lk(guardMutex);
 	        	guardCV.wait(lk);
 	        }
-		}
+		} else {
+			facade->runAll();
+			facade->updateWorld();
 
-		facade->runAll();
-		facade->updateWorld();
+			/* Logging AI & World */
+			if(updatesOccured()){
+				if(aiLogger != NULL){
+					aiLogger->log("\n####################");
+					aiLogger->log("Step " + to_string(simulationTime));
+					aiLogger->log("Nb agents \t= " + to_string(facade->listAgent.size()));
+					aiLogger->log("Nb tribes \t= " + to_string(facade->listTribe.size()));
+					aiLogger->log("Nb objects \t= " + to_string(facade->listIE.size()));
 
-		/* Logging AI & World */
-		if(updatesOccured()){
-			if(aiLogger != NULL){
-				aiLogger->log("\n####################");
-				aiLogger->log("Step " + to_string(simulationTime));
-				aiLogger->log("Nb agents \t= " + to_string(facade->listAgent.size()));
-				aiLogger->log("Nb tribes \t= " + to_string(facade->listTribe.size()));
-				aiLogger->log("Nb objects \t= " + to_string(facade->listIE.size()));
+					// Log changes of agents
+					aiLogger->log("____________________");
+					aiLogger->log("Updated agents");
+					for(Sentient_Entity * a : facade->listAgent){
+						if(a->getModif()){
+							aiLogger->log(a->getName());
+						}
+					}
 
-				// Log changes of agents
-				aiLogger->log("____________________");
-				aiLogger->log("Updated agents");
-				for(Sentient_Entity * a : facade->listAgent){
-					if(a->getModif()){
-						aiLogger->log(a->getName());
+					// Log changes of tribes
+					aiLogger->log("____________________");
+					aiLogger->log("Updated tribes");
+					for(Tribe * t : facade->listTribe){
+						if(t->getModif()){
+							aiLogger->log(t->getName());
+						}
 					}
 				}
 
-				// Log changes of tribes
-				aiLogger->log("____________________");
-				aiLogger->log("Updated tribes");
-				for(Tribe * t : facade->listTribe){
-					if(t->getModif()){
-						aiLogger->log(t->getName());
+				// Log changes to the world
+					aiLogger->log("____________________");
+					aiLogger->log("Updated objects");
+				if(worldLogger != NULL){
+					for(Entity* e : facade->getUpdatedAgents()){
+						worldLogger->log(e->getName());
 					}
 				}
 			}
 
-			// Log changes to the world
-				aiLogger->log("____________________");
-				aiLogger->log("Updated objects");
-			if(worldLogger != NULL){
-				for(Entity* e : facade->getUpdatedAgents()){
-					worldLogger->log(e->getName());
-				}
+			/* Broadcasting changes */
+
+			// TODO
+
+
+			/* Reset update flags of world elements (agents, objects, tribes) */
+			for(Sentient_Entity * a : facade->listAgent){
+				a->setModif(false);
 			}
+			for(Tribe * t : facade->listTribe){
+				t->setModif(false);
+			}
+			for(Insentient_Entity * ie : facade->listIE){
+				ie->setModif(false);
+			}
+
+			// Increase time counter
+			simulationTime++;
+			simulationTime %= simulationTimeWarp;
 		}
-
-		/* Broadcasting changes */
-
-		// TODO
-
-
-		/* Reset update flags of world elements (agents, objects, tribes) */
-		for(Sentient_Entity * a : facade->listAgent){
-			a->setModif(false);
-		}
-		for(Tribe * t : facade->listTribe){
-			t->setModif(false);
-		}
-		for(Insentient_Entity * ie : facade->listIE){
-			ie->setModif(false);
-		}
-
-		// Increase time counter
-		simulationTime++;
-		simulationTime %= simulationTimeWarp;
 	}
 }
 
@@ -163,16 +160,18 @@ void WorldSimulator::handleUserCommands(){
 				} else {
 					cout << "\tMissing parameter \"file\". Usage : load <file>" << endl;
 				}
-			} else if(words[0].compare("pause") == 0){
-				if(state != SimulationState::PAUSED_SIMULATION){
+			} else if(words[0].compare("pause") == 0 || words[0].compare("p") == 0){
+				if(state == SimulationState::RUNNING_SIMULATION){
 					state = SimulationState::PAUSED_SIMULATION;
+					cout << "\tPaused" << endl;
 				} else {
 					cout << "\tThe simulation is already paused" << endl;
 				}
-			} else if(words[0].compare("resume") == 0){
+			} else if(words[0].compare("resume") == 0 || words[0].compare("r") == 0){
 				if(state == SimulationState::PAUSED_SIMULATION){
 					lock_guard<mutex> lk(guardMutex);
 					state = SimulationState::RUNNING_SIMULATION;
+					cout << "\tResumed" << endl;
 					guardCV.notify_one();
 				} else {
 					cout << "\tYou can only resume the simulation when it is paused" << endl;
@@ -183,8 +182,18 @@ void WorldSimulator::handleUserCommands(){
 				} else {
 					cout << "\tMissing parameter \"file\". Usage : save <file>" << endl;
 				}
-			} else if(words[0].compare("stop") == 0){
-				state = SimulationState::STOPPING_SIMULATION;
+			} else if(words[0].compare("stop") == 0 || words[0].compare("s") == 0){
+				if(state == SimulationState::PAUSED_SIMULATION){
+					{
+						lock_guard<mutex> lk(guardMutex);
+						state = SimulationState::STOPPING_SIMULATION;
+					}
+
+				    // Allow simulation to resume
+				    guardCV.notify_one();
+				} else {
+					state = SimulationState::STOPPING_SIMULATION;
+				}
 			} else {
 				cout << "\tUnknown command \"" << words[0] << "\". You can list available commands by typing \"help\"" << endl;
 			}
@@ -200,10 +209,10 @@ void WorldSimulator::displayUserCommands(){
 	cout << "Available commands :" << endl;
 	cout << "\thelp, h, ?\t: Displays this help" << endl;
 	cout << "\tload <file>\t: Stops the simulation and replaces it with the simulation in <file>" << endl;
-	cout << "\tpause\t\t: Pauses the simulation" << endl;
-	cout << "\tresume\t\t: Resumes the simulation if it was paused" << endl;
+	cout << "\tpause, p\t: Pauses the simulation" << endl;
+	cout << "\tresume, r\t: Resumes the simulation if it was paused" << endl;
 	cout << "\tsave <file>\t: Saves the current state of the simulation in <file>" << endl;
-	cout << "\tstop\t\t: Stops the simulation and exits the program" << endl;
+	cout << "\tstop, s\t\t: Stops the simulation and exits the program" << endl;
 }
 
 bool WorldSimulator::updatesOccured(){
@@ -211,29 +220,32 @@ bool WorldSimulator::updatesOccured(){
 }
 
 void WorldSimulator::save(const string fileName){
-	state = SimulationState::REQUIRE_WAIT_SIMULATION;
+	SimulationState prevState = state;
+	if(state != SimulationState::PAUSED_SIMULATION){
+		state = SimulationState::REQUIRE_WAIT_SIMULATION;
 
-	// Wait for server step to finish
-	{
-		unique_lock<mutex> lk(guardMutex);
-	    guardCV.wait(lk);
+		// Wait for server step to finish
+		{
+			unique_lock<mutex> lk(guardMutex);
+		    guardCV.wait(lk);
+		}
 	}
 
 
 
-	cout << "Saving... " << endl;
+	cout << "\tSaving... " << endl;
     // TODO save world data to file
 
 
 	this_thread::sleep_for( chrono::milliseconds( 2000 ));
 
 
-	cout << "Done saving" << endl;
+	cout << "\tDone saving" << endl;
 
 	// Signal simulation that the saving process is now over
 	{
 		lock_guard<mutex> lk(guardMutex);
-	    state = SimulationState::RUNNING_SIMULATION;
+	    state = prevState;
 	}
 
 
@@ -243,29 +255,32 @@ void WorldSimulator::save(const string fileName){
 }
 
 void WorldSimulator::load(const string fileName){
-	state = SimulationState::REQUIRE_WAIT_SIMULATION;
+	SimulationState prevState = state;
+	if(state != SimulationState::PAUSED_SIMULATION){
+		state = SimulationState::REQUIRE_WAIT_SIMULATION;
 
-	// Wait for server step to finish
-	{
-		unique_lock<mutex> lk(guardMutex);
-	    guardCV.wait(lk);
+		// Wait for server step to finish
+		{
+			unique_lock<mutex> lk(guardMutex);
+		    guardCV.wait(lk);
+		}
 	}
 
 
 
-	cout << "Loading... " << endl;
+	cout << "\tLoading... " << endl;
     // TODO load world data from file
 
 
 	this_thread::sleep_for( chrono::milliseconds( 2000 ));
 
 
-	cout << "Done Loading" << endl;
+	cout << "\tDone Loading" << endl;
 
 	// Signal simulation that the loading process is now over
 	{
 		lock_guard<mutex> lk(guardMutex);
-	    state = SimulationState::RUNNING_SIMULATION;
+	    state = prevState;
 	}
 
 
